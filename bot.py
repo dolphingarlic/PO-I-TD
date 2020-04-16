ef6848ad22179d3a3b519a9474620c43687f909c
Andi Qu <dolphingarlic@gmail.com>
2020-04-16 14:48:51 +0200

    Added ability to ask for a problem at any time

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
+'''
+PO(I)TD
+
+A Discord bot that sends a random OI problem every day
+'''
+
 import os
 import asyncio
-
 from datetime import datetime, timedelta
+
 import requests
 import discord
 from discord.ext import commands, tasks
@@ -14,14 +20,20 @@ BOT = commands.Bot("$")
 CHANNEL = None
 
 
-@tasks.loop(hours=24)
 async def random_problem():
+    '''
+    Gets a random problem from APIOIPA and sends it as an embed
+    '''
+
     print('Sending problem!')
     global CHANNEL
-    problem = requests.get('https://apioipa.herokuapp.com/random-problem/').json()
-    source = requests.get(f'https://apioipa.herokuapp.com/sources/{problem["source"]}').json()['abbreviation']
+    problem = requests.get(
+        'https://apioipa.herokuapp.com/random-problem/').json()
+    source = requests.get(
+        f'https://apioipa.herokuapp.com/sources/{problem["source"]}').json()['abbreviation']
 
-    embed = discord.Embed(title=f'{source} {problem["from_year"]} - {problem["name"]}', color=0x00ff00)
+    embed = discord.Embed(
+        title=f'{source} {problem["from_year"]} - {problem["name"]}', color=0x00ff00)
     embed.url = problem['url']
     embed.description = 'Problem of the Day'
     embed.set_image(url=problem['image'])
@@ -29,8 +41,33 @@ async def random_problem():
     await CHANNEL.send(embed=embed)
 
 
-@random_problem.before_loop
-async def before_random_problem():
+@BOT.command()
+async def gimme(ctx):
+    '''
+    Sends a random problem to the user in the channel where the command was sent
+    '''
+
+    global CHANNEL
+    CHANNEL = ctx.message.channel
+    await random_problem()
+    CHANNEL = BOT.get_channel(627064201797697543)
+
+
+@tasks.loop(hours=24)
+async def potd():
+    '''
+    Sends a random problem every day
+    '''
+
+    await random_problem()
+
+
+@potd.before_loop
+async def before_potd():
+    '''
+    Makes sure the bot sends a random problem every day at 8:20
+    '''
+
     await BOT.wait_until_ready()
 
     hour = 8
@@ -46,11 +83,15 @@ async def before_random_problem():
 
 @BOT.event
 async def on_ready():
+    '''
+    Called when the bot is ready
+    '''
+
     print(f'{BOT.user} has connected to Discord!')
     global CHANNEL
     CHANNEL = BOT.get_channel(627064201797697543)
 
 
-random_problem.start()
+potd.start()
 
 BOT.run(TOKEN)
